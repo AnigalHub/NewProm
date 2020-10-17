db639cb4bf96925fa0b54f9f004d1b0511c9a8d2
Ignatova Anna <anigal1997@gmail.com>
2020-10-17 03:13:49 +0300

    скелет страницы - основные принципы СМК

---
+++ src/components/smk.vue
@@ -0,0 +1,75 @@
+<template>
+    <div id="smk">
+        <b-container>
+            <b-row>
+                <b-col cols="7">
+                    <h2>Система менеджмента качества организации</h2>
+                    <p class="text">
+                        Система менеджмента качества организации разработана, задокументирована, внедрена, поддерживается в рабочем состоянии и соответствует требованиям ГОСТ Р ИСО 9001-2015, ГОСТ РВ 0015-002-2012, ЭС РД 09-2014, ЭС РД 010-2015, что подтверждается сертификатом соответствия, выданным Органом по сертификации систем менеджмента качества Автономной некоммерческой организацией "Центр испытаний и сертификации "Промтехносерт" (СДС "Электронсерт").
+                    </p>
+                    <b-table  hover :items="smk"></b-table>
+                    <b-row>
+                        <b-col>
+                            <img src="../../public/images/iso17025.png">
+                            <p class="text">ГОСТ Р ИСО 9001-2015</p>
+                        </b-col>
+                        <b-col>
+                            <img src="../../public/images/ввт.png">
+                            <p class="text">ГОСТ РВ 0015-002-2012</p>
+                        </b-col>
+                        <b-col>
+                            <img src="../../public/images/эс.png">
+                            <p class="text">СДС "Электронсерт"</p>
+                        </b-col>
+                    </b-row>
+                </b-col>
+                <b-col>
+                    <img src="../../public/images/smk.jpg" >
+                </b-col>
+            </b-row>
+        </b-container>
+    </div>
+</template>
+
+<script>
+    import  smk from '../../public/documents/smk';
+    export default {
+        name: "smk",
+        data() {
+            return {
+                smk: smk,
+            }
+        }
+    }
+</script>
+
+<style>
+    .table td, .table th{
+         font-weight: 600 !important;
+            padding: 1% 1% 1% 15% ;
+    }
+    .table th, .table td {
+        border: 1px solid rgba(18, 23, 37, 0.71) !important;
+        background: rgba(255, 255, 255, 0.52);
+
+    }
+    tr:hover{
+        background: rgb(124, 150, 169) !important;
+    }
+    .table thead th {
+        vertical-align: middle !important;
+        background: rgb(121, 147, 164) !important;
+        color: white !important;
+    }
+</style>
+<style scoped lang="scss">
+    #smk{
+        background: url("../../public/фон.jpg");
+        background-size: 100% 100%;
+        padding: 1% 0 2.5% 0;
+    }
+    .col-7 .col{
+        padding: 0 3% 0 3%;
+    }
+
+</style>
