58ba6e2483faaf550639ca48240b6d542516585e
Ignatova Anna <anigal1997@gmail.com>
2020-10-21 23:12:32 +0300

    добавлен вывод всех таблиц

--- src/components/supply/program.vue
+++ src/components/supply/program.vue
@@ -7,13 +7,13 @@
                     <b-table sticky-header hover :items="active_components"></b-table>
                 </b-tab>
                 <b-tab title="Пассивные компоненты">
-
+                    <b-table sticky-header hover :items="passive_components"></b-table>
                 </b-tab>
                 <b-tab title="Соединители и Разъемы">
-
+                    <b-table sticky-header hover :items="connectors"></b-table>
                 </b-tab>
                 <b-tab title="Дисплеи и Светодиоды">
-
+                    <b-table sticky-header hover :items="displays"></b-table>
                 </b-tab>
             </b-tabs>
         </b-container>
@@ -22,13 +22,17 @@
 
 <script>
     import  active_components from '../../../public/documents/active_components';
-
+    import  passive_components from '../../../public/documents/passive_components';
+    import  connectors from '../../../public/documents/connectors';
+    import  displays from '../../../public/documents/displays';
     export default {
         name: "program",
         data() {
             return {
                 active_components:active_components,
-
+                passive_components:passive_components,
+                connectors:connectors,
+                displays:displays,
             }
         }
     }
@@ -37,7 +41,6 @@
     .tab-content {
         border: 1.5px solid #183018  !important;
         border-radius: 10px;
-        padding: 1%;
     }
     .nav-pills .nav-link.active {
         background-color: #3e5262 !important;
@@ -47,9 +50,10 @@
         color: #183018 !important;
         border: 1.5px solid rgba(62, 82, 98, 0.65) !important;
         background: rgba(255, 255, 255, 0.13);
+        padding: 0.2rem !important;
     }
     .table th, .table td {
-        padding: 0.1% !important;
+        padding: 0.25% !important;
         border: 1px solid rgba(18, 23, 37, 0.71) !important;
         text-align: center !important;
         background: rgba(255, 255, 255, 0.72);
@@ -67,7 +71,7 @@
         text-shadow: 0.25px 0.25px .5px rgba(18, 23, 37, 0.71);
     }
     .b-table-sticky-header {
-        max-height: 60vh !important;
+        max-height: 65vh !important;
     }
     tr:hover{
         background: rgb(91, 114, 131) !important;
@@ -78,5 +82,11 @@
         background: url("../../../public/фон.jpg");
         background-size: 100% 100%;
         padding: 1% 0 2.5% 0;
+    }
+    .tab-content{
+        padding: 5% !important;
+    }
+    .b-table-sticky-header{
+        margin: 0 !important;
     }
 </style>
