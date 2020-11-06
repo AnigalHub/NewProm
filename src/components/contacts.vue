fa7d20bb31415c3b5dabe0fd201a5cef332e1530
Ignatova Anna <anigal1997@gmail.com>
2020-11-06 19:02:57 +0300

    удалены лишние стили, перенесены в общие для всех

--- src/components/contacts.vue
+++ src/components/contacts.vue
@@ -69,14 +69,6 @@
 </script>
 
 <style scoped lang="scss">
-    #contacts{
-        background: url("../../public/фон.jpg");
-        background-size: 100% 100%;
-        padding: 1% 0 2.5% 0;
-    }
-    h2{
-        color: rgba(5, 35, 71, 0.75);
-    }
     svg{
         width: 52px;
         height: 52px;
