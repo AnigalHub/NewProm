59d4529603d3617bbbad3aee1c0c5549da1c463d
Ignatova Anna <anigal1997@gmail.com>
2020-11-08 12:14:24 +0300

    удалены стили для tabов,перенесены в общие - App

--- src/components/news.vue
+++ src/components/news.vue
@@ -168,32 +168,8 @@
         }
     }
 </script>
-<style>
-    .tab-content {
-        border: 1.5px solid #183018  !important;
-        border-radius: 10px;
-    }
-    .nav-pills .nav-link.active {
-        background-color: #3e5262 !important;
-        color: white !important;
-    }
-    .nav-pills .nav-link {
-        color: #183018 !important;
-        border: 1.5px solid rgba(62, 82, 98, 0.65) !important;
-        background: rgba(62, 82, 98, 0.15);
-        padding: 0.2rem !important;
-    }
 
-    .b-table-sticky-header {
-        max-height: 65vh !important;
-    }
-</style>
 <style scoped lang="scss">
-    #news{
-        background: url("../../public/фон.jpg");
-        background-size: 100% 100%;
-        padding: 1% 0 2.5% 0;
-    }
     .row{
         background: rgba(255, 255, 255, 0.79);
         border-bottom: 2px solid #839eb0;
