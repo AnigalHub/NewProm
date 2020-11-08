ad317039792874fd6eaa06c8dba4bb456aa01ccd
Ignatova Anna <anigal1997@gmail.com>
2020-11-08 12:22:44 +0300

    добавлены стили для всех компонентов - на table,tabs,изменен путь до фона

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,6 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Navbar from "./components/navbar";
 import Home from "./components/LandingPage/home";
 import Advantages from "./components/LandingPage/advantages";
@@ -31,7 +30,6 @@ export default {
       Advantages,
       Home,
       Navbar,
-    HelloWorld
   }
 }
 </script>
@@ -39,6 +37,50 @@ export default {
   ul{
     margin-block-start: 0 !important;
     padding-inline-start: 5px !important;
+  }
+  .table th, .table td {
+    padding: 0.1% !important;
+    border: 1px solid rgba(18, 23, 37, 0.71) !important;
+    text-align: center !important;
+    background: rgba(255, 255, 255, 0.72);
+  }
+  .table thead th {
+    vertical-align: middle !important;
+    max-width: 20% !important;
+    background: rgb(91, 114, 131) !important;
+  }
+  th {
+    color: white !important;
+    text-shadow: 1.5px 1.5px 2px rgba(18, 23, 37, 0.71);
+  }
+  td{
+    text-shadow: 0.25px 0.25px .5px rgba(18, 23, 37, 0.71);
+  }
+  .b-table-sticky-header {
+    max-height: 70vh !important;
+  }
+  tr:hover{
+    background: rgb(91, 114, 131) !important;
+  }
+  .tab-content {
+    border: 1.5px solid #183018  !important;
+    border-radius: 10px;
+    padding: 0.8%;
+    background: rgba(255, 255, 255, 0.53);
+  }
+  .nav-pills .nav-link.active {
+    background-color: #3e5262 !important;
+    color: white !important;
+  }
+  .nav-pills .nav-link {
+    color: #183018 !important;
+    border: 1.5px solid rgba(62, 82, 98, 0.65) !important;
+    background: rgba(62, 82, 98, 0.15);
+    padding: 0.2rem !important;
+  }
+
+  .b-table-sticky-header {
+    max-height: 65vh !important;
   }
 </style>
 <style lang="scss">
@@ -55,8 +97,8 @@ export default {
   }
   #company,#laboratory, #vhodnoi_control,#dop_trials,#climatic_trials,#certification_trials,
   #mehanic_trials,#supply,#pcb_materials,#manufactures,#program,#selection_and_supply_it,#package_services,
-  #equipment,#guarantee,#smk,#use_smk,#events_smk,#news,#contacts, #advantages,#stages{
-    background: url("../public/фон.jpg");
+  #equipment,#guarantee,#smk,#use_smk,#events_smk,#news,#contacts, #advantages,#stages,#mycontact{
+    background: url("../public/images/background/фон.jpg");
     background-size: 100% 100%;
     padding: 1% 0 2% 0;
   }
@@ -125,10 +167,12 @@ export default {
   }
   tr,th{
       font-family: "Open Sans",Arial,sans-serif;
-      color: #161640 !important;
       font-size: 1rem !important;
       line-height: 1.5rem;
       cursor: default;
+  }
+  tr{
+    color: #161640 !important;
   }
   h6{
       font-weight: 700 !important;
