793ee7dc6c137ec6103cdf112bafd84163f330f0
Ignatova Anna <anigal1997@gmail.com>
2020-11-09 20:10:59 +0300

    исправлены ошибки в шаблоне, добавлено название и индексы (id)

--- src/components/trials_stages.vue
+++ src/components/trials_stages.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
             <b-card no-body>
-                <b-card-header header-tag="header" class="p-1" role="tab" v-b-toggle.accordion-1>
+                <b-card-header header-tag="header" class="p-1" role="tab" v-b-toggle="toogle_name + index">
                     {{stage_name}}
                 </b-card-header>
-                <b-collapse id="accordion-1" visible accordion="my-accordion" role="tabpanel">
+                <b-collapse :id="toogle_name + index" visible accordion="my-accordion" role="tabpanel">
                     <b-card-body>
                         <b-card-text>{{ stage_descr }}</b-card-text>
                     </b-card-body>
@@ -19,6 +19,14 @@
         props:{
             stage_name: String,
             stage_descr: String,
+            index: {
+                type: Number,
+                default: 0,
+            },
+            toogle_name:{
+                type: String,
+                default: "accordion",
+            }
         },
     }
 </script>
