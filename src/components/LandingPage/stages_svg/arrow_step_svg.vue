323b05b11a48165bfe005be554f31a473c6646f3
AnigalHub <anigal1997@gmail.com>
2020-11-18 23:24:59 +0300

    class заменен на id

--- src/components/LandingPage/stages_svg/arrow_step_svg.vue
+++ src/components/LandingPage/stages_svg/arrow_step_svg.vue
@@ -1,5 +1,5 @@
 <template>
-    <svg class="arrow_step" width="174px" height="120px" version="1.0" viewBox="0 0 174 70" xmlns="http://www.w3.org/2000/svg">
+    <svg id="arrow_step" width="174px" height="120px" version="1.0" viewBox="0 0 174 70" xmlns="http://www.w3.org/2000/svg">
         <g class="hover" transform="translate(0 70) scale(.1 -.1)">
             <path d="m1220 617c0-3 21-47 47-98 26-52 49-97 51-101 2-5-294-8-657-8-581 0-661-2-661-15s79-15 653-15c360 0 656-2 659-5s-17-48-44-101c-28-52-48-97-46-99s44 28 93 66c109 86 110 86 210 125l79 31-54 18c-80 26-126 51-209 114-40 31-84 64-97 74s-24 17-24 14z">
             </path>
@@ -14,5 +14,4 @@
 </script>
 
 <style scoped lang="scss">
-
 </style>
