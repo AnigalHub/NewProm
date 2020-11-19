5c8d25c3a1b334ec04f62b55b07fa8b2ce073925
AnigalHub <anigal1997@gmail.com>
2020-11-20 00:17:06 +0300

    добавлен вывод адреса,телефона и почты с помощью шалона company_address

--- src/components/LandingPage/mycontact.vue
+++ src/components/LandingPage/mycontact.vue
@@ -4,19 +4,9 @@
             <h2>Контакты</h2>
             <b-row>
                 <b-col cols="5" class="text">
-                    <div><component :is="landSVG"/>{{address}}</div>
-                    <div><component :is="metroSVG"/>{{metro}}</div>
-                    <div><component :is="opening_hoursSVG"/>{{opening_hours}}</div>
-                    <div>
-                        <component :is="telephoneSVG"/><a :href="'tel:'+telephone">{{telephone}}</a>,<br>
-                        <component :is="faxSVG"/><a :href="'tel:'+fax">{{fax}}(факс)</a>,<br>
-                        <component :is="receptionSVG"/><a :href="'tel:'+reception">{{reception}}(приемная секретаря)</a>
-                    </div>
-                    <div>
-                        <a :href="'mailto:'+mail"><component :is="mailSVG"/>{{mail}}</a>
-                    </div>
+                   <CompanyAddress></CompanyAddress>
                 </b-col>
-                <b-col >
+                <b-col>
                     <div id="mymap">
                         <div class="feature map" id="map-wrap">
                             <iframe :src="map"></iframe>
@@ -29,40 +19,14 @@
 </template>
 
 <script>
-    import LandSVG from './address_svg/land_svg';
-    import MetroSVG from './address_svg/metro_svg';
-    import OpeningHoursSVG from './address_svg/opening_hours_svg';
-    import TelephoneSVG from './address_svg/telephone_svg';
-    import FaxSVG from './address_svg/fax_svg';
-    import ReceptionSVG from './address_svg/reception_svg';
-    import MailSVG from './address_svg/mail_svg';
-    import address from '../../public/documents/address/address.json';
-    import metro from '../../public/documents/address/metro.json';
-    import opening_hours from '../../public/documents/address/opening_hours.json';
-    import telephone from '../../public/documents/address/telephone.json';
-    import fax from '../../public/documents/address/fax.json';
-    import reception from '../../public/documents/address/reception.json';
-    import mail from '../../public/documents/address/mail.json';
-    import map from '../../public/documents/address/map.json';
+
+    import CompanyAddress from "../company_address";
+    import map from "../../../public/documents/address/map.json";
     export default {
-        components: {LandSVG,MetroSVG,OpeningHoursSVG,TelephoneSVG,FaxSVG,ReceptionSVG,MailSVG},
         name: "mycontact",
+        components: {CompanyAddress},
         data(){
             return{
-                landSVG:LandSVG,
-                metroSVG:MetroSVG,
-                opening_hoursSVG:OpeningHoursSVG,
-                telephoneSVG:TelephoneSVG,
-                faxSVG:FaxSVG,
-                receptionSVG:ReceptionSVG,
-                mailSVG:MailSVG,
-                address:address,
-                metro:metro,
-                opening_hours:opening_hours,
-                telephone:telephone,
-                fax:fax,
-                reception:reception,
-                mail:mail,
                 map:map,
             }
         }
@@ -74,20 +38,11 @@
         color: #121224;
         text-align: center !important;
     }
-    svg{
-        width: 48px;
-        height: 48px;
-    }
-    .text,a{
-        color: #121224;
-    }
-
     .col-5{
         border: 2px solid #121224;
-        padding: 1.5%;
+        padding: 0.8%;
         border-radius: 15px;
     }
-
     .feature.map {
         iframe {
             min-height: 410px;
@@ -97,20 +52,29 @@
             box-shadow: none !important;
         }
     }
-    @media screen and (max-width: 500px) {
 
+    @media screen and  (max-width: 768px) {
+        .row{
+            flex-direction: column;
+            .col-5{
+                max-width: 100%;
+            }
+            .col{
+                padding: 0 !important;
+            }
+        }
         .feature.map {
             iframe {
-                min-height: 250px !important;
+                margin-top: 1.5%;
+                min-height: 380px !important;
                 border-radius: 0.625rem !important;
             }
         }
     }
-    @media screen and (min-width: 500px)and (max-width: 768px) {
-
+    @media screen and (min-width: 768px)and (max-width: 992px) {
         .feature.map {
             iframe {
-                min-height: 330px !important;
+                min-height: 360px !important;
                 border-radius: 0.625rem !important;
             }
         }
