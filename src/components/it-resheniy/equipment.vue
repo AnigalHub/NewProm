ad78a88ba23c394a5b9ed94ef45d74d14bd41199
AnigalHub <anigal1997@gmail.com>
2020-11-22 18:09:48 +0300

    добавлен шаблон на скачивание таблицы

--- src/components/it-resheniy/equipment.vue
+++ src/components/it-resheniy/equipment.vue
@@ -3,6 +3,7 @@
         <b-container>
             <h2>Перечень поставляемого оборудования</h2>
             <div class="place_for_table">
+                <pdf_and_xlsx></pdf_and_xlsx>
                 <b-table sticky-header hover :items="it"></b-table>
             </div>
         </b-container>
@@ -11,8 +12,10 @@
 
 <script>
     import  it from '../../../public/documents/it';
+    import Pdf_and_xlsx from "../pdf_and_xlsx";
     export default {
         name: "equipment",
+        components: {Pdf_and_xlsx},
         data() {
             return {
                 it: it,
